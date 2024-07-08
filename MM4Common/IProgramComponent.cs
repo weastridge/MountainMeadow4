404c4280254287469512f3069d68e3350607a493
weastridge <weastridge@users.noreply.github.com>
2024-07-08 16:06:08 -0400

    making a base program component

---
+++ MM4Common/IProgramComponent.cs
@@ -0,0 +1,15 @@
+
+namespace MM4Common
+{
+    /// <summary>
+    /// component to plug into the MM4 program
+    /// </summary>
+    public interface IProgramComponent
+    {
+        /// <summary>
+        /// hosts a panel to show in MM4 Display area if any. (nullable)
+        /// </summary>
+        public System.Windows.Controls.UserControl? Display { get; set; }
+    }
+
+}
---
+++ PCBase/BaseProgramComponent.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MM4Common;
+
+namespace PCBase
+{
+    /// <summary>
+    /// base template for a MM4 program component
+    /// </summary>
+    public class BaseProgramComponent:IProgramComponent
+
+    {
+        /// <summary>
+        /// the display which hosts a panel, (nullable)
+        /// </summary>
+        public UserControl? Display { get; set; }
+    }
+
+}
